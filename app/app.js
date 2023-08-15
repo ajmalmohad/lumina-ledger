9f5f88b5a525d6ad2e5c384c2bb1694776b967a5
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-08-15 15:50:00 +0530

    Add block to chain

--- app/app.js
+++ app/app.js
@@ -5,16 +5,26 @@ import Blockchain from './../blockchain/blockchain.js'
 const port  = process.env.PORT || 3000;
 dotenv.config();
 const app = express();
+app.use(express.json());
 const blockchain = new Blockchain();
 
 app.get('/', (req, res) => {
-  res.send('Working!')
+  res.send('Working!');
 })
 
 app.get('/blocks', (req, res) => {
-    res.send(blockchain.chain)
+    res.send(blockchain.chain);
+})
+
+app.post('/mine', (req, res) => {
+  let data = req.body.data;
+  if(!data) res.status(404).send({"error": "Data not recieved"});
+  else{
+    blockchain.addBlock(data);
+    res.status(200).send(blockchain.chain);
+  }
 })
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}`)
+  console.log(`Listening on port ${port}`);
 })
