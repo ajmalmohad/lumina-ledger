b695416c05e9a840c40ce533d98714654305ef78
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-08-15 15:24:48 +0530

    API for access

--- app/app.js
+++ app/app.js
@@ -2,13 +2,19 @@ import dotenv from 'dotenv'
 import express from 'express'
 import Blockchain from './../blockchain/blockchain.js'
 
-dotenv.config()
-const app = express()
+const port  = process.env.PORT || 3000;
+dotenv.config();
+const app = express();
+const blockchain = new Blockchain();
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Working!')
 })
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Listening on port ${process.env.PORT || 3000}`)
+app.get('/blocks', (req, res) => {
+    res.send(blockchain.chain)
+})
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`)
 })
