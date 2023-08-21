b5ec44cbbb0ec3cb66c828d4ebc11cb886b375dd
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-08-21 22:07:45 +0530

    Transaction Input Test

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -18,6 +18,10 @@ describe('Transaction', () => {
         expect(transaction.outputs.find(output => output.address === recipient).amount).toEqual(amount);
     });
 
+    it('inputs the balance of the wallet', ()=>{
+        expect(transaction.input.amount).toEqual(wallet.balance);
+    });
+
     describe('transacting with an amount that exceeds the balance', () => { 
         beforeEach(()=>{
             amount = 5000;
