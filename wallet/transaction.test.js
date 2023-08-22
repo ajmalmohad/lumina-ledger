d430c0fd513940270821cf6d4e06d8b079baf6a2
ajmalmohad <ajmalajmal.2016@gmail.com>
2023-08-22 23:55:55 +0530

    Update Transactions and Tests

--- wallet/transaction.test.js
+++ wallet/transaction.test.js
@@ -41,4 +41,21 @@ describe('Transaction', () => {
             expect(transaction).toEqual(undefined);
         });
      })
+
+    describe('and updating a transaction', () => { 
+        let nextAmount, nextRecipient;
+        beforeEach(()=>{
+            nextAmount = 20;
+            nextRecipient = 'next-recipient';
+            transaction = transaction.update(wallet, nextRecipient, nextAmount);
+        });
+    
+        it('subtracts next amount from the sender\'s output', ()=>{
+            expect(transaction.outputs.find(output => output.address === wallet.publicKey).amount).toEqual(wallet.balance - amount - nextAmount);
+        });
+
+        it('outputs an amount for the next recipient', ()=>{
+            expect(transaction.outputs.find(output => output.address === nextRecipient).amount).toEqual(nextAmount);
+        });
+     })
  })
